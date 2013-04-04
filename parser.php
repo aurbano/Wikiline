cb3e49ca689dcf3c3cfffaa5ccea7adfc40a2399
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2013-04-04 11:51:41 +0200

    Adding offline mode with sample data

--- parser.php
+++ parser.php
@@ -10,6 +10,7 @@ include('lib/wikipedia.api.php');	// Load Wikipedia php API
 
 class Parser{
 	private $wiki;
+	private $events;
 	
 	public function Parser(){
 		$this->wiki = new WikipediaAPI();
@@ -41,14 +42,41 @@ class Parser{
 		}
 		// Make sure the crawl url is properly formatted
 		$url = str_replace(' ','_',$next->url);*/
+		
+		/* ONLINE
 		$url = 'Joseph_Clay_Stiles_Blackburn';
 		// Pull article from Wikipedia
 		$data = json_decode($this->wiki->request($url,'parse','text'),true);
 		
+		$toParse = strip_tags($data['parse']['text']['*']);
+		*/
+		
+		/* OFFLINE */
+		$toParse = file_get_contents('data/joseph.txt');
+		
+		$lines = explode("\n",$toParse);
+		for($i=0;$i<count($lines);$i++){
+			// I would like to get date ranges
+			// as well as unique dates.
+			// Ranges tend to be year - year, or full date - full date
+			// unique dates tend to be (YYYY-MM-DD) although it might vary
+			$res = array();
+			preg_match('@\(([0-9]{2,4})-([0-9]{1,2})-([0-9]{1,2})\)@',$lines[$i],$res);
+			if(count($res)>0){
+				$this->add($lines[$i-1],$res[1]);
+			}
+			
+			if(strlen(trim($lines[$i]))<1) continue;
+			echo $lines[$i];
+			if(strtotime($lines[$i])>0) echo '{Date}';
+			echo '<br />';
+		}
+		
+		die();
 		// Birth info regex
 		$res = array();
-		preg_match('@(Joseph (.*) Blackburn) \((january|february|march|april|may|june|july|august|september|october|november|december) ([0-9]{1,2}),? ([0-9]{2,4}) (–|0x2013|-)? (january|february|march|april|may|june|july|august|september|october|november|december) ([0-9]{1,2}),? ([0-9]{2,4})\)@i', strip_tags($data['parse']['text']['*']), $res);
-		print_r($res);
+		//preg_match('@(Joseph (.*) Blackburn) \((january|february|march|april|may|june|july|august|september|october|november|december) ([0-9]{1,2}),? ([0-9]{2,4}) (?:.+)? (january|february|march|april|may|june|july|august|september|october|november|december) ([0-9]{1,2}),? ([0-9]{2,4})\)@i', $$toParse, $res);
+		var_dump($res);
 		die();
 		$total = count($res);
 		echo 'total:'.$total;
@@ -56,6 +84,16 @@ class Parser{
 		$date2 = $res[$total-3].' '.$res[$total-2].', '.$res[$total-1];
 		echo '<br />Event: '.$res[1].':: ';
 		echo $date1.' -- '.$date2;
+	}
+	
+	/**
+	 * Adds an event to the internal list
+	 */
+	function add($name, $date){
+		$this->events[] = array(
+			'name' => $name,
+			'date' => $date
+		);
 	}
 };
 // Same as crawl here
