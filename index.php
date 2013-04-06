9475aab116f7d3caee4f9a04fc25768f44c734d9
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2013-04-06 21:19:14 +0200

    Initial design

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Wikiline - Global timeline</title>
+<link rel="stylesheet" type="text/css" media="all" href="layout.css" />
+<link rel="stylesheet" type="text/css" media="all" href="timeline.css" />
+</head>
+
+<body>
+<header>
+	<h1>Wikiline</h1>
+</header>
+<div id="timeline-viewport">
+	<div id="timeline">
+		<div class="item entertainment" style="left:100px;">
+			<div class="info">
+				<div class="content">
+					<h3>A very important event</h3>
+					<article>Description, if any</article>
+				</div>
+			</div>
+		</div>
+		<hr class="line" />
+		<div class="tick" style="left:500px"><span>1900</span></div>
+	</div>
+</div>
+<footer>Wikiline - Global timeline</footer>
+</body>
+</html>
