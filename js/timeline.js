51eb1603f90d582a1ffaa1babb892457a972759b
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2013-04-12 14:51:21 +0200

    Mosaic display of events

--- js/timeline.js
+++ js/timeline.js
@@ -1,5 +1,8 @@
 /**
  * Timeline class, for interacting and generating the Timeline
+ * the real timeline should be a group of views, each spanning some years
+ * to allow horizontal scrolling without drawing too many elements
+ *
  * @author Alejandro U. Alvarez <alejandro@urbanoalvarez.es> 
  */
 
@@ -12,17 +15,36 @@ var Timeline = {
 		end : 2000
 	},
 	/**
-	 * Minimum offset between drawn events
-	 */
-	combine : 140,
-	/**
 	 * Events buffer 
 	 */
 	events : new Array(),
 	/**
+	 * Rows array, holds the offset of each row
+	 * if the website resizes vertically, for now, ignore
+	 */
+	rows : new Array(),
+	rowHeight : 50,
+	/**
+	 * Stores DOM references, trying to improve rendering speed
+	 */
+	refs : new Array(),
+	/**
+	 * Zoom level, currently it will be distance in px between ticks
+	 */
+	zoom : 100,
+	/**
 	 * Draw resolution 
 	 */
 	resolution : 10,
+	/**
+	 * Kind of an overflow method for jQuery selector
+	 * checks local cache and calls jquery if not found
+	 */
+	$ : function(selector){
+		if(Timeline.refs[selector] !== undefined) return Timeline.refs[selector];
+		Timeline.refs[selector] = $(selector);
+		return Timeline.refs[selector];
+	},
 	/**
 	 * Display ticks on timeline 
 	 */
@@ -34,29 +56,45 @@ var Timeline = {
 		// Determine tick separation
 			space = Math.floor($('#timeline').width()/ticks);
 		for(var i=0;i<ticks;i++){
-			$('#timeline').append('<div class="tick" style="left:'+(i*space)+'px"><span>'+(Timeline.interval.start+Timeline.resolution*i)+'</span></div>');
+			Timeline.$('#timeline').append('<div class="tick" style="left:'+(i*space)+'px"><span>'+(Timeline.interval.start+Timeline.resolution*i)+'</span></div>');
 		}
+	},
+	/**
+	 * Determines how many rows to use, and initializes the array
+	 */
+	createRows : function(){
+		// Should be called once everything has been resized
+		var height = Timeline.$('#events').height(),
+			numRows = Math.floor(height/Timeline.rowHeight) - 1;		
+		for(i=0;i<numRows;i++){
+			Timeline.rows[i] = 0;
+		}
+		console.log('Generated '+i+' rows for events');
 	},
 	/**
 	 * Main function, should be called on document ready 
 	 */
 	main : function(){
+		//Build references
+		Timeline.refs['#timeline-viewport'] = $('#timeline-viewport');
+		Timeline.refs['#timeline'] = $('#timeline');
+		Timeline.refs['#events'] = $('#events');
+		
+		// Initialize timeline
 		Timeline.resize();
 		Timeline.drawTicks();
+		Timeline.createRows();
+		// Display data
 		Timeline.loadEvents();
 	},
-	lastEvent : {
-		offset : 0,
-		obj : null
-	},	
 	/**
 	 * Draws an event on the timeline 
 	 * @param Object An object with all event properties
 	 */
 	drawEvent : function(evt){
-		console.log('Drawing event:');
-		console.log(evt);
-		// Test boundaries
+		// Notify
+		console.log('Drawing ['+Timeline.dateToString(evt.date)+'] '+evt.title);
+		// Test boundaries (just in case, although probably impossible to get wrong)
 		if(evt.date.y < Timeline.interval.start || evt.date.y > Timeline.interval.end){
 			console.log('Event '+evt.title+' out of bounds');
 			return;
@@ -65,39 +103,41 @@ var Timeline = {
 		/*
 		 * Calculating the offset: First the date is converted to a decimal number
 		 * in years, where the decimals represent months, days... etc
+		 * We will then try to find the row that best suits this offset, although it could
+		 * vary.
 		 */
 		var year = Timeline.dateToDigit(evt.date) - Timeline.interval.start,
 			max = Timeline.interval.end - Timeline.interval.start,
-			offset = year * $('#events').width() / max;
-		
-		// Decide whether create a new item or mix with previous
-		if(Timeline.lastEvent.obj !== null){
-			// Now check offset
-			if(offset - Timeline.lastEvent.offset < Timeline.combine){
-				console.log('Combining with last, offset = '+Timeline.lastEvent.offset);
-				// Combine events
-				Timeline.lastEvent.obj.append(Timeline.createEvent(evt));
-				return;
+			offset = year * $('#events').width() / max,
+			nearestRow = 0,
+			nearestDiff = 1000;
+		console.log('	Offset = '+offset);
+
+		// Calculate nearest row
+		for(i=0;i<Timeline.rows.length;i++){
+			if(Timeline.rows[i] < offset){
+				nearestRow = i;
+				break;
+			}
+			if(Math.abs(offset - Timeline.rows[i]) < nearestDiff){
+				nearestRow = i;
+				nearestDiff = Math.abs(Timeline.rows[i] - offset);
 			}
 		}
 		
-		var height = 500 + Math.random()*300;
+		// Move right if necessary
+		if(Timeline.rows[nearestRow] < offset) Timeline.rows[nearestRow] = offset;
+		
+		// Now draw in that row
+		
+		var id = evt.title.replace(' ','_');
 		
 		// Now draw the element
-		var obj = $('<div class="item '+evt.type+'" style="left:'+offset+'px;"><div class="info up" style="height:'+height+'px; top:-'+height+'px"><div class="content">'+Timeline.createEvent(evt)+'</div></div></div>').appendTo('#events').find('.content');
+		var obj = $('<a href="#" class="event row'+nearestRow+'" style="top:'+(nearestRow*Timeline.rowHeight)+'px; left:'+(Timeline.rows[nearestRow])+'px; background:'+Timeline.randColor()+'" id="'+id+'" title="'+evt.title+'"><h3>'+evt.entity+'</h3><article>'+evt.title+'</article><time>'+Timeline.dateToString(evt.date)+'</time></a>').appendTo('#events');
 		
-		// Store data for next drawing
-		Timeline.lastEvent.offset = offset;
-		Timeline.lastEvent.obj = obj;
-		
-		return offset;
-	},
-	/**
-	 *
-	 */
-	createEvent : function(evt){
-		var id = evt.title.replace(' ','_');
-		return '<div class="event" id="'+id+'"><h3>'+evt.entity+'</h3><article>'+evt.title+'</article><time>'+Timeline.dateToString(evt.date)+'</time></div>';
+		// Update that row's offset
+		Timeline.rows[nearestRow] = obj.offset().left + obj.width() + 5;
+				
 	},
 	/**
 	 * Converts a date object to a digit
@@ -141,12 +181,23 @@ var Timeline = {
 		"json");
 	},
 	/**
+	 * Returns a random color
+	 */
+	randColor : function(){
+		return '#'+Math.round(0xffffff * Math.random()).toString(16);
+	},
+	/**
 	 * Maintain viewport in full size
 	 */
 	resize : function(){
-		$('#timeline-viewport').width($(window).width());
-		$('#timeline-viewport').height($(window).height());
+		Timeline.$('#timeline-viewport').width($(window).width());
+		Timeline.$('#timeline-viewport').height($(window).height());
 		
-		$('#events').height($(window).height() - $('#timeline').height() - 5);
+		var ticks = Math.ceil((Timeline.interval.end - Timeline.interval.start)/Timeline.resolution);
+		
+		Timeline.$('#timeline').width(Timeline.zoom * ticks);
+		Timeline.$('#events').width(Timeline.zoom * ticks);
+		
+		Timeline.$('#events').height($(window).height() - Timeline.$('#timeline').height() - 5);
 	}
 };
